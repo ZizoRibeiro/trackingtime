2aa5b09d38f7dae53f7f2694b2fbab2f362688fa
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-08-22 13:07:32 -0700

    added edit funcionallity status to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 @user = User.create(email: "john@example.com", password: "abcabcabc", password_confirmation: "abcabcabc",
                     first_name: "John", last_name: "Doe")
 
+puts "1 User Created"
+
+AdminUser.create(email: "zizo@example.com", password: "abcabcabc", password_confirmation: "abcabcabc",first_name: "zizo", last_name: "Rib")
 
 puts "1 User Created"
 
