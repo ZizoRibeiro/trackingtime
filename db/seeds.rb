cd36f544b52bdc1fa151e5d69558ffe39300630c
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-08-31 19:45:18 -0700

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,16 @@ AdminUser.create(email: "zizo@example.com",
                  last_name: "Rib",
                  phone: "7786539058")
 
-puts "1 User Created"
+puts "1 Admin User Created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, tracking_hours: 2.5)
 end
 
-100.times do |post|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+puts "100 posts have been created"
+
+100.times do |audit_log|
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
-puts "100 posts have been created"
+puts "100 audit logs have been created"
