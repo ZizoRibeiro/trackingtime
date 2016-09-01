7de10cdfb7a5918e1dc3d2665dc39cdf7475905f
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-08-31 20:15:13 -0700

    Implemented audit log tab that is shown only to admin users

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,4 +4,18 @@ class PostPolicy < ApplicationPolicy
     return true if record.approved? && admin_types.include?(user.type)
     return true if (record.user_id == user.id || admin_types.include?(user.type)) && !record.approved?
   end
+
+  private
+
+    def user_or_admin
+      record.user_id == user.id || admin?
+    end
+
+    def admin?
+      admin_types.include?(user.type)
+    end
+
+    def post_approved?
+      record.approved?
+    end
 end
