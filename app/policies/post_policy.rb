5f9fdc4e5bfd6bb9935d5d0391ca0f4fedb33f2d
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-09-06 16:30:58 -0700

    Added approval and review buttons for admin review homepage

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -5,6 +5,10 @@ class PostPolicy < ApplicationPolicy
     return true if (record.user_id == user.id || admin_types.include?(user.type)) && !record.approved?
   end
 
+  def approve?
+    admin?
+  end
+
   private
 
     def user_or_admin
