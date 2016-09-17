c2d46f6dedc0e6c4ff39a9bbeaae19a0701290e8
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-09-17 13:56:44 -0700

    Added self reference model for user manager/employee feature resolves #2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
     end
 
-    xit 'requires the phone attr to only have 10 chars' do
+    it 'requires the phone attr to only have 10 chars' do
       @user.phone = '12345678901'
       expect(@user).to_not be_valid
     end
@@ -47,4 +47,16 @@ RSpec.describe User, type: :model do
       expect(@user.full_name).to eq("DOE, JOHN")
     end
   end
+
+  describe 'relationship between admins and employees' do
+    it 'allows for admins to be associated with multiple employees' do
+      employee_1 = FactoryGirl.create(:user)
+      employee_2 = FactoryGirl.create(:user)
+      admin = FactoryGirl.create(:admin_user)
+      Hand.create(user_id: admin.id, hand_id: employee_1.id)
+      Hand.create(user_id: admin.id, hand_id: employee_2.id)
+      expect(admin.hands.count).to eq(2)
+    end
+  end
+
 end
